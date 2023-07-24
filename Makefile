1e8531bcc35e1254ea3dc17105127f6e6f477f3f
Jim Huang <jserv@ccns.ncku.edu.tw>
2023-07-24 20:06:01 +0800

    Evaluate IR Framework

--- Makefile
+++ Makefile
@@ -1,8 +1,22 @@
+CXX ?= g++
+
 LIBMIR = mir/libmir.a
 $(LIBMIR): mir/mir.c
 	$(MAKE) -C mir
 mir/mir.c:
 	git clone https://github.com/vnmakarov/mir
+
+ir/ir.o: ir/ir.c
+	$(MAKE) -C ir BUILD=release
+ir/ir.c:
+	git clone https://github.com/dstogov/ir
+
+IR_OBJS = \
+    ir/ir.o ir/ir_strtab.o ir/ir_cfg.o \
+    ir/ir_sccp.o ir/ir_gcm.o ir/ir_ra.o ir/ir_emit.o \
+    ir/ir_load.o ir/ir_save.o ir/ir_emit_c.o ir/ir_dump.o \
+    ir/ir_disasm.o ir/ir_gdb.o ir/ir_perf.o ir/ir_check.o \
+    ir/ir_cpuinfo.o
 
 utest:
 	git clone https://github.com/sheredom/utest.h $@
@@ -10,12 +24,18 @@ utest:
 .DEFAULT_GOAL :=
 all: main
 
-main: $(LIBMIR) utest jit-mir.cc main.cc
-	g++ -o $@ \
-		-I./mir -I. \
+CXXFLAGS = -O3 -Wall
+CXXFLAGS += -DIR_TARGET_X64
+CXXFLAGS += -Imir -I.
+
+main: $(LIBMIR) utest jit-mir.cc main.cc ir/ir.o
+	$(CXX) -o $@ $(CXXFLAGS) \
 		jit-mir.cc \
 		main.cc \
 		$(LIBMIR) \
+		$(IR_OBJS) \
+		-lcapstone \
+		-ldl \
 		-lpthread
 
 clean:
--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@ namespace
 struct JitC {
     cjit::JitCompiler *compiler;
 };
-}
+}  // namespace
 
 #define JITC (utest_fixture->compiler)
 
@@ -87,7 +87,49 @@ UTEST_F(JitC, simple_micro)
     ASSERT_EQ(13, fun(2, 3));
 }
 
-UTEST_F(JitC, mandelbrot)
+typedef int (*mandelbrot_t)(double, double);
+static void run(mandelbrot_t mandelbrot)
+{
+    fflush(stdout);
+    for (int y = -39; y < 39; y++) {
+        printf("\n");
+        for (int x = -39; x < 39; x++) {
+            int i = mandelbrot(x / 40.0, y / 40.0);
+            printf("%c", i ? ' ' : '*');
+        }
+    }
+    printf("\n");
+}
+
+static int mandelbrot_c(double x, double y)
+{
+    double cr = y - 0.5;
+    double ci = x;
+    double zi = 0.0;
+    double zr = 0.0;
+    int i = 0;
+
+    while (1) {
+        i++;
+        double temp = zr * zi;
+        double zr2 = zr * zr;
+        double zi2 = zi * zi;
+        zr = zr2 - zi2 + cr;
+        zi = temp + temp + ci;
+        if (zi2 + zr2 > 16)
+            return i;
+        if (i > 1000)
+            return 0;
+    }
+}
+
+UTEST_F(JitC, mandelbrot_static)
+{
+    run(mandelbrot_c);
+    ASSERT_TRUE(1);
+}
+
+UTEST_F(JitC, mandelbrot_mir)
 {
     auto *compiler = JITC;
     const char *c_code =
@@ -115,18 +157,88 @@ UTEST_F(JitC, mandelbrot)
 
     cjit::CompiledInfo info = compiler->compile(c_code);
 
-    typedef int (*fun_p)(double, double);
-    fun_p fun = (fun_p) info.binary;
+    run((mandelbrot_t) info.binary);
+    ASSERT_TRUE(info.binary);
+}
 
-    for (int y = -39; y < 39; y++) {
-        printf("\n");
-        for (int x = -39; x < 39; x++) {
-            int i = fun(x / 40.0, y / 40.0);
-            printf("%c", i ? ' ' : '*');
-        }
-    }
-    printf("\n");
-    ASSERT_TRUE(fun);
+#include "ir/ir.h"
+#include "ir/ir_builder.h"
+
+static void gen_mandelbrot(ir_ctx *ctx)
+{
+    ir_START();
+    /* clang-format off */
+    ir_ref x = ir_PARAM(IR_DOUBLE, "x", 1);
+    ir_ref y = ir_PARAM(IR_DOUBLE, "y", 2);
+    ir_ref cr = ir_SUB_D(y, ir_CONST_DOUBLE(0.5));
+    ir_ref ci = ir_COPY_D(x);
+    ir_ref zi = ir_COPY_D(ir_CONST_DOUBLE(0.0));
+    ir_ref zr = ir_COPY_D(ir_CONST_DOUBLE(0.0));
+    ir_ref i = ir_COPY_D(ir_CONST_I32(0));
+
+    ir_ref loop = ir_LOOP_BEGIN(ir_END());
+        ir_ref zi_1 = ir_PHI_2(zi, IR_UNUSED);
+        ir_ref zr_1 = ir_PHI_2(zr, IR_UNUSED);
+        ir_ref i_1 = ir_PHI_2(i, IR_UNUSED);
+
+        ir_ref i_2 = ir_ADD_I32(i_1, ir_CONST_I32(1));
+        ir_ref temp = ir_MUL_D(zr_1, zi_1);
+        ir_ref zr2 = ir_MUL_D(zr_1, zr_1);
+        ir_ref zi2 = ir_MUL_D(zi_1, zi_1);
+        ir_ref zr_2 = ir_ADD_D(ir_SUB_D(zr2, zi2), cr);
+        ir_ref zi_2 = ir_ADD_D(ir_ADD_D(temp, temp), ci);
+        ir_ref if_1 = ir_IF(ir_GT(ir_ADD_D(zi2, zr2), ir_CONST_DOUBLE(16.0)));
+            ir_IF_TRUE(if_1);
+                ir_RETURN(i_2);
+            ir_IF_FALSE(if_1);
+                ir_ref if_2 = ir_IF(ir_GT(i_2, ir_CONST_I32(1000)));
+                ir_IF_TRUE(if_2);
+                    ir_RETURN(ir_CONST_I32(0));
+                ir_IF_FALSE(if_2);
+                    ir_ref loop_end = ir_LOOP_END();
+
+    /* close loop */
+    ir_MERGE_SET_OP(loop, 2, loop_end);
+    ir_PHI_SET_OP(zi_1, 2, zi_2);
+    ir_PHI_SET_OP(zr_1, 2, zr_2);
+    ir_PHI_SET_OP(i_1, 2, i_2);
+    /* clang-format on */
+}
+
+UTEST_F(JitC, mandelbrot_ir)
+{
+    uint32_t mflags = 0;
+    uint32_t cpuinfo = ir_cpuinfo();
+    if (cpuinfo & IR_X86_AVX)
+        mflags |= IR_X86_AVX;
+
+    uint32_t flags = IR_OPT_FOLDING | IR_OPT_CFG | IR_OPT_CODEGEN;
+    ir_ctx ctx;
+    ir_init(&ctx, flags, 256, 1024);
+    ctx.mflags = mflags;
+
+    gen_mandelbrot(&ctx);
+    ir_build_def_use_lists(&ctx);
+    ir_sccp(&ctx);
+    ir_build_cfg(&ctx);
+    ir_build_dominators_tree(&ctx);
+    ir_find_loops(&ctx);
+    ir_gcm(&ctx);
+    ir_schedule(&ctx);
+    ir_match(&ctx);
+    ir_assign_virtual_registers(&ctx);
+    ir_compute_live_ranges(&ctx);
+    ir_coalesce(&ctx);
+    ir_reg_alloc(&ctx);
+    ir_schedule_blocks(&ctx);
+    ir_truncate(&ctx);
+
+    size_t size;
+    void *entry = ir_emit_code(&ctx, &size);
+    run((mandelbrot_t) entry);
+
+    ir_free(&ctx);
+    ASSERT_TRUE(size);
 }
 
 UTEST_MAIN()
